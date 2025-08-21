2be914f038594b079b48e2dd923566c0ecc2e36d
Abhishek Jha <abhiishekk9887@gmail.com>
2025-08-21 09:58:22 +0545

    chnage

--- secondFunction.js
+++ secondFunction.js
@@ -1,38 +1,22 @@
+"use strict";
 // while we write function like this 
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // function addVal(num){
 //     // so either I am giving number it allow to uppercase cause it accept any 
 //     num.toUpperCase;
 //     return num + 2;
 // }
-
-
 // addVal(90)
-
 // export {}
-
 // fine way to do 
-
-function addVal(num:number){
+function addVal(num) {
     return num + 2;
 }
-
-
-addVal(90)
-
-function getUpper(val:String){
-    return val.toUpperCase()
+addVal(90);
+function getUpper(val) {
+    return val.toUpperCase();
 }
-
-getUpper("Abhis")
-
-
-
-function signUp(name:string, email:string, isPaid:boolean){
-
+getUpper("Abhis");
+function signUp(name, email, isPaid) {
 }
-
-signUp("a","s", false)
-
-
-export {}
+signUp("a", "s", false);
