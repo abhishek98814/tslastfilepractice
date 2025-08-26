0c0c6b72c121d7cdff4f3332c0530d665e1accc7
Abhishek Jha <abhiishekk9887@gmail.com>
2025-08-26 08:54:07 +0545

    change

---
+++ myArray.ts
@@ -0,0 +1,20 @@
+const superHero : string[] = [];
+const numberArray : Array<number> = []
+
+superHero.push("ABHI")
+numberArray.push(9000)
+
+
+type User = {
+    name : string
+    isActive: boolean
+}
+
+
+
+const MLModel: number[][] = [
+    [255,355,677]
+]
+
+
+
---
+++ readOnly.ts
@@ -0,0 +1,28 @@
+type User = {
+    readonly _id: string
+    name:string
+    email: string
+    isActive:boolean
+     homeTown?:string
+}
+
+function createUser (u: User){
+
+}
+
+
+let myUser: User = {
+    _id: "1234",
+    name: "Abhis",
+    email:"don",
+    isActive: false,
+   
+}
+
+
+myUser.email = "ABHI@gmail.com"
+// it not allow cause we give there read only
+// myUser._id = "hr"
+
+
+export {}
