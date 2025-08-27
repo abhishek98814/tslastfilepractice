51d882e1132e1827d695b26dea52eaebf5b5776d
Abhishek Jha <abhiishekk9887@gmail.com>
2025-08-27 09:52:42 +0545

    change

---
+++ enum.ts
@@ -0,0 +1,18 @@
+// enum in number behave default as increment but it is not compulsory
+// we can put string too and string number too
+
+enum  sheera{
+    Aisle = 90,
+    Middle,
+    window,
+    lastVal = 99
+}
+
+// the valur of window if 92 
+// but if i declare myself the value will obviously change 
+
+
+enum lastDecalaration {
+    first="Abhishek",
+    second=90
+}
